d9a12d4918f6c30285f6d9ee845636471e03caf9
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-03-10 12:59:54 +0000

    updated tag specification in docker build

--- build-container.sh
+++ build-container.sh
@@ -1,14 +1,19 @@
 set -e
 
-sudo docker build -f Dockerfile . -t metadata:tsv
+tag=$1
 
-sudo docker run -v /home/jose_almeida/projects/metadata-sequence-classification/metadata_tmp:/data:ro metadata:tsv /data/metadata-from-stelios.tsv
+sudo docker build -f Dockerfile . -t metadata:$tag
+
+echo Testing with test_jose.tsv
+sudo docker run -v /home/jose_almeida/projects/metadata-sequence-classification/metadata_tmp:/data:ro metadata:$tag /data/test_jose.tsv > /dev/null
+echo Testing with ecrfs-series-20230309.parquet
+sudo docker run -v /home/jose_almeida/projects/metadata-sequence-classification/metadata_tmp:/data:ro metadata:$tag /data/ecrfs-series-20230309.parquet > /dev/null
 
 sudo docker tag \
-    metadata:tsv \
-    pcr.procancer-i.eu/metadata-classification/metadata-classification:tsv
+    metadata:$tag \
+    pcr.procancer-i.eu/metadata-classification/metadata-classification:$tag
 
 sudo docker login pcr.procancer-i.eu
 
 sudo docker push \
-    pcr.procancer-i.eu/metadata-classification/metadata-classification:tsv
+    pcr.procancer-i.eu/metadata-classification/metadata-classification:$tag
