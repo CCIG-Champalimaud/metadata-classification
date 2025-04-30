67dc600d13658d738b9d103a7aa61ba4399a8187
josegcpa <jose.almeida@research.fchampalimaud.pt>
2025-04-30 08:50:15 +0100

    removing redundant predict scripts

--- predict.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python -m src.msc.utils.predict \
-    --model_paths \
-    models/catboost.percent_phase_field_of_view:sar:series_description.pkl \
-    --input_paths "${@:1}"
